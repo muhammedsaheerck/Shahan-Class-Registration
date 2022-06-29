dcc8431d776f83141882aed11bad6226c45621c9
muhammedsaheerck <108222206+muhammedsaheerck@users.noreply.github.com>
2022-06-29 16:15:46 +0530

    Update form.php

--- form.php
+++ form.php
@@ -9,7 +9,7 @@
     $name = $_POST['time'];
     $name = $_POST['days'];
     $message = $_POST['message'];
-    $from = 'From: http://127.0.0.1:5500/web/index.html';
+    $from = 'From: https://muhammedsaheerck.github.io/Shahan-Class-Registration/';
     $to = 'saheersaheer212@gmail.com';
     $subject = 'Customer Inquiry';
     $body = "From: $name\n E-Mail: $email\n Message:\n $message";
